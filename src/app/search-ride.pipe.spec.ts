c45663a1468e08e804d62a68c3d8b90fc05115c0
= <kumarnvijay71@gmail.com>
2020-01-16 10:07:40 -0500

    Auto-generated commit

--- src/app/search-ride.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchRidePipe } from './search-ride.pipe';
-
-describe('SearchRidePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchRidePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
